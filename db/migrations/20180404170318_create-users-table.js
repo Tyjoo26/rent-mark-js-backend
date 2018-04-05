c6d8ac3cb3dba9b09a93af63906d2d8117dd9b5d
Tyjoo26 <Tyjoo21@gmail.com>
2018-04-05 08:25:03 -0600

    Added bcrypt, migrated users table, no relationships

--- db/migrations/20180404170318_create-users-table.js
+++ db/migrations/20180404170318_create-users-table.js
@@ -2,10 +2,15 @@
 exports.up = function(knex, Promise) {
   return knex.schema
   .createTable("users", function(t){
-    t.increments('id')
+    t.increments('id').unsigned().primary();
+    t.string("first_name").notNullable();
+    t.string("last_name").notNullable();
+    t.string("email").notNullable();
+    t.string("password").notNullable();
+    t.enum('access', ["tenant", "manager"]).notNullable();
   })
 };
 
 exports.down = function(knex, Promise) {
-
+  return knex.schema.dropTable("users")
 };
