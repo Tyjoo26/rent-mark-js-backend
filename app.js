2f253dd1fbf98c78ffb6e84f9e4b13b5a2d388fa
Tyjoo26 <Tyjoo21@gmail.com>
2018-04-25 12:29:21 -0600

    add email service

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ var logger = require('morgan');
 var cors = require('cors')
 
 
+// using SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'tyjoo21@gmail.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
+
+
 var indexRouter = require('./routes/index');
 
 var apiV1UsersRouter = require('./routes/api/v1/users-router');
